361de18104c2ad658249b710775b256cf4d4d490
dpett122 <dpett122@gmail.com>
2018-09-26 21:36:58 -0400

    reorganizing

--- src/pages/State.js
+++ src/pages/State.js
@@ -1,5 +1,9 @@
-import { Link } from 'react-router-dom';
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import classnames from 'classnames';
+
+// bootstrap
 import {
   TabContent,
   TabPane,
@@ -9,19 +13,22 @@ import {
   Col,
   Table,
 } from 'reactstrap';
-import {
-  FacebookShareButton,
-  TwitterShareButton,
-  EmailShareButton
-} from 'react-share';
-import classnames from 'classnames';
-import ReactToPrint from 'react-to-print';
 
 // dictionary
 import { stateInfo } from '../utils/stateInfo.js';
 
 // components
-import Candidates from '../components/Candidates.js'
+import Candidates from '../components/Candidates.js';
+
+// share buttons
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  EmailShareButton
+} from 'react-share';
+
+// print functionality
+import ReactToPrint from 'react-to-print';
 
 // icons
 import { IconContext } from 'react-icons';
@@ -33,20 +40,20 @@ import { IoIosArrowBack, IoIosArrowUp, IoIosArrowDown } from 'react-icons/io';
 import { colors } from '../utils/colors'
 import { constants } from '../utils/constants'
 
-// State
+
 const State = props => {
   // props: stateId
   const { id } = props.match.params;
   const newId = id.toUpperCase();
   return (
     <div className='pageContainer'>
-      <StateContainer stateId={newId} />
+      <StatePage stateId={newId} />
     </div>
   );
 };
 
-// State > StateContainer
-class StateContainer extends React.Component {
+
+class StatePage extends React.Component { // State -> StatePage
   constructor(props) {
     super(props); // props: stateId
     this.state = {
@@ -55,11 +62,18 @@ class StateContainer extends React.Component {
       card: true,
       showHints: false
     };
-    this.toggle = this.toggle.bind(this);
-    this.handleChange = this.handleChange.bind(this);
   }
 
-  hintTimeoutShow() {
+  componentDidMount() {
+    this.hintDisplayTimer() // show hints
+    this.setState({
+      // set activeTab to whatever the first legislature is
+      activeTab: stateInfo[this.props.stateId].legislatures[0].id
+    });
+  }
+
+  hintDisplayTimer = () => {
+    // timeout to show hints temporarily
     new Promise((resolve, reject) => {
       setTimeout(
         function () {
@@ -67,76 +81,72 @@ class StateContainer extends React.Component {
           document.addEventListener('touchstart', this.handleClickOutside);
           document.addEventListener('touchmove', this.handleClickOutside);
           this.setState({ showHints: true })
-        }
-          .bind(this),
+        }.bind(this),
         100
       )
-    }).then(this.hintTimeoutHide())
+    }).then(
+      setTimeout(
+        function () {
+          document.removeEventListener('mousedown', this.handleClickOutside);
+          document.removeEventListener('touchstart', this.handleClickOutside);
+          document.removeEventListener('touchmove', this.handleClickOutside);
+          this.setState({ showHints: false })
+        }.bind(this),
+        2400
+      )
+    )
   }
 
-  hintTimeoutHide() {
-    setTimeout(
-      function () {
-        document.removeEventListener('mousedown', this.handleClickOutside);
-        document.removeEventListener('touchstart', this.handleClickOutside);
-        document.removeEventListener('touchmove', this.handleClickOutside);
-        this.setState({ showHints: false })
-      }
-        .bind(this),
-      2400
-    );
-  }
-  async componentDidMount() {
-    // set activeTab to whatever the first legislature is, in case usHouse is not up for election in this state
-    await this.hintTimeoutShow()
-    this.setState({
-      activeTab: stateInfo[this.props.stateId].legislatures[0].id
-    });
+  setWrapperRef = (node) => {
+    // assign hint text ref
+    this.wrapperRef = node;
   }
 
   handleClickOutside = (event) => {
+    // handle clicks outside of hints
     if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
       this.setState({
+        // dismiss hints
         showHints: false
       })
     }
   }
-  setWrapperRef = (node) => {
-    this.wrapperRef = node;
+
+  handleChange = (type, selected, remove) => {
+    // handle candidate checkbox click
+    const { pressed } = this.state
+    const incomingCandidateArray = selected.split(',');
+    const index = this.getIndexOfState(pressed, incomingCandidateArray);
+
+    switch (type) {
+      case 'add':
+        pressed.push(incomingCandidateArray);
+        break;
+      case 'delete':
+        pressed.splice(index, 1);
+        break;
+      case 'change':
+        pressed.splice(this.getIndexOfState(pressed, remove.split(',')), 1, incomingCandidateArray);
+        break;
+      default:
+        console.log('switch defaulted');
+        break;
+    }
+    this.setState({ pressed });
   }
 
-  indexGet(pressed, arr) {
+  getIndexOfState = (pressed, incomingCandidateArray) => {
+    // check this.state.pressed for new pressed
     for (var i = 0; i < pressed.length; i++) {
-      let index = pressed[i].indexOf(arr[3]);
+      let index = pressed[i].indexOf(incomingCandidateArray[3]);
       if (index > -1) {
         return +i;
       }
     }
   }
 
-  handleChange(type, selected, remove) {
-    const arr = selected.split(',');
-    const index = this.indexGet(this.state.pressed, arr);
-    const pressed = this.state.pressed;
-
-    switch (type) {
-      case 'add':
-        pressed.push(arr);
-        break;
-      case 'delete':
-        pressed.splice(index, 1);
-        break;
-      case 'change':
-        pressed.splice(this.indexGet(this.state.pressed, remove.split(',')), 1, arr);
-        break;
-      default:
-        console.log('switch defaulted');
-        break;
-    }
-    this.setState({ pressed: pressed });
-  }
-
-  toggle(tab) {
+  toggle = (tab) => {
+    // toggle tab for legislature
     if (this.state.activeTab !== tab) {
       this.setState({
         activeTab: tab
@@ -144,79 +154,66 @@ class StateContainer extends React.Component {
     }
   }
 
-  toggleCard() {
-    if (this.state.card) {
-      this.setState({
-        card: false
-      })
-    }
-    else {
-      this.setState({
-        card: true
-      })
-    }
+  toggleCard = () => {
+    // toggle voting card popup (for mobile)
+    this.setState({
+      card: !this.state.card
+    })
   }
 
   render() {
     const stateName = stateInfo[this.props.stateId].name;
     const legislatures = stateInfo[this.props.stateId].legislatures;
 
-    // Nav Tabs
     const navTabs = stateInfo[this.props.stateId]['legislatures'].map(x => (
-      // x = stateInfo.{stateId}.legislatures[i]
+      // Nav Tabs (mapped) x = stateInfo.{stateId}.legislatures[i]
       <NavItem key={x.id}>
         <NavLink
-          className={classnames({
-            active: this.state.activeTab === x.id
-          })}
-          onClick={() => {
-            this.toggle(x.id);
-          }}
+          className={classnames({ active: this.state.activeTab === x.id })}
+          onClick={() => { this.toggle(x.id) }}
         >
           {x.name}
         </NavLink>
       </NavItem>
     ));
 
-    // Tab Panes
     const tabPanes = legislatures.map(x => (
-      // x = stateInfo.{stateId}.legislatures[i]
+      // Tab Panes (mapped) x = stateInfo.{stateId}.legislatures[i]
       <TabPane key={x.id + 'tabpane'} tabId={x.id}>
         {x.districts.map(y => (
-          // x = stateInfo.{stateId}.legislatures[i]districts[i]
+          // Candidates (mapped) x = stateInfo.{stateId}.legislatures[i]districts[i]
           <div key={y.name + 'district'}>
-            <p style={districtNameInTabs}>
+            <p style={Styles.districtNameInTabs}>
               {x.name} – {y.name}
             </p>
-            <div>
-              <div className='candidatesContainer' style={candidatesContainer}>
-                <Candidates
-                  candidates={y.candidates}
-                  legislature={x.name}
-                  district={y.name}
-                  handleChange={this.handleChange}
-                />
-              </div>
+            <div className='candidatesContainer' style={Styles.candidatesContainer}>
+              <Candidates
+                candidates={y.candidates}
+                legislature={x.name}
+                district={y.name}
+                handleChange={this.handleChange}
+                handleClickOutside={this.handleClickOutside}
+              />
             </div>
           </div>
         ))}
       </TabPane>
     ));
 
-    // Format Candidate metainfo for Voting Card
     const shareQuote = this.state.pressed.map(
-      y => '\n' + y[0] + `${y[1] === 'Statewide' ? '' : ' – ' + y[1]}` + ': ' + y[2] + ' (' + y[4] + ')'
+      // Format Candidate metainfo for Voting Card
+      y => '\n' + y[0] + y[1] === 'Statewide' ? '' : ' – ' + y[1] + ': ' + y[2] + ' (' + y[4] + ')'
     );
 
     return (
       <div id='StateContainer' style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
 
-        <div ref={this.setWrapperRef} id={this.state.showHints ? 'scrollToTopHint' : 'scrollToTopHintHidden'} style={scrollToTopHint}>
-          <div id='scrollToTopHintArrow' style={scrollToTopHintArrow} />
+        <div ref={this.setWrapperRef} id={this.state.showHints ? 'popupButtonHint' : 'popupButtonHintHidden'} style={Styles.popupButtonHint}>
+          <div id='popupButtonHintArrow' style={Styles.popupButtonHintArrow} />
           Click here to view your voting card.
         </div>
 
-        <button id='scrollToTop' style={scrollToTop} onClick={() => this.toggleCard()}>
+        <button id='popupButton' style={Styles.popupButton} onClick={() => this.toggleCard()}>
           <IconContext.Provider value={{ color: colors.primary, size: '2.2em', className: 'global-class-name' }}>
             {
               this.state.card ?
@@ -230,6 +227,7 @@ class StateContainer extends React.Component {
             }
           </IconContext.Provider>
         </button>
+
 
         <Col className='noMarginCol' md='7'>
           <div className='funbox funboxBorder'>
@@ -385,18 +383,18 @@ class StateContainer extends React.Component {
 class MyVotes extends React.Component {
   render() {
     const votes = this.props.pressed.map(y => (
-      <tr style={cardRow} key={y[3] + 'votes'}>
+      <tr key={y[3] + 'votes'}>
         <td>
-          <img style={cardImage} src={y[3]} alt={y[2]} />
+          <img style={Styles.cardImage} src={y[3]} alt={y[2]} />
         </td>
         <td style={{ lineHeight: '1.2' }}>
           <div className='text-muted hintText'>
             {y[0]}{y[1] === 'Statewide' ? '' : ' - ' + y[1]}
           </div>
-          <div style={cardName}>
+          <div style={Styles.cardName}>
             {y[2]} ({y[4] === 'Republican' || y[4] === 'Democrat' || y[4] === 'Green' || y[4] === 'Libertarian' ? y[4][0] : 'I'})
           </div>
-          <div style={cardGrade}>
+          <div style={Styles.cardGrade}>
             {y[5]}
           </div>
         </td>
@@ -437,91 +435,79 @@ class MyVotes extends React.Component {
   }
 }
 
-
-const candidatesContainer = {
-  width: '100%',
-  display: 'grid',
-  // gridTemplateColumns: 'repeat(2, 1fr)',
-  gridColumnGap: 10,
-  gridRowGap: 10
-  // gridGap: '10px',
-  // gridAutoRows: 'minMax(100px, auto)'
-}
-
-const scrollToTop = {
-  cursor: 'pointer',
-  position: 'fixed',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  // border: `1px solid ${colors.primary}`,
-  width: 55,
-  height: 55,
-  border: '1px solid rgba(0,0,0,0)',
-  // right: window.innerHeight - 100,
-  // top: window.innerWidth - 100
-  bottom: 10,
-  right: 10,
-  borderRadius: '50%',
-  zIndex: 2,
-  backgroundColor: colors.primaryLight
-}
-
-const scrollToTopHint = {
-  position: 'fixed',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  paddingLeft: 10,
-  paddingRight: 10,
-  paddingTop: 8,
-  paddingBottom: 8,
-  bottom: (10 + 55) + 15,
-  right: (10) + 5,
-  borderRadius: '3px',
-  backgroundColor: 'white',
-  zIndex: 2,
-}
-const scrollToTopHintArrow = {
-  bottom: '-10px',
-  right: 15,
-  border: '5px solid white',
-  content: '',
-  position: 'absolute',
-  borderBottomColor: 'transparent',
-  borderRightColor: 'transparent',
-  borderTopColor: 'white',
-  borderLeftColor: 'transparent'
-}
-
-const districtNameInTabs = {
-  marginTop: 30,
-  marginBottom: 8,
-  fontSize: 16,
-}
-
-const cardImage = {
-  width: 41,
-  height: 53,
-  borderRadius: 4,
-  border: `1px solid grey`,
-  marginTop: 3,
-  marginBottom: 3,
-}
-
-const cardName = {
-  fontSize: '.9rem'
-}
-
-const cardGrade = {
-  fontSize: '1.1rem',
-  fontWeight: 600,
-}
-
-const cardParty = {
-  fontSize: '.7rem'
-}
-
-const cardRow = {
+const Styles = {
+  candidatesContainer: {
+    width: '100%',
+    display: 'grid',
+    // gridTemplateColumns: 'repeat(2, 1fr)',
+    gridColumnGap: 10,
+    gridRowGap: 10
+    // gridGap: '10px',
+    // gridAutoRows: 'minMax(100px, auto)'
+  },
+  districtNameInTabs: {
+    marginTop: 30,
+    marginBottom: 8,
+    fontSize: 16,
+  },
+  cardImage: {
+    width: 41,
+    height: 53,
+    borderRadius: 4,
+    border: `1px solid grey`,
+    marginTop: 3,
+    marginBottom: 3,
+  },
+  cardName: {
+    fontSize: '.9rem'
+  },
+  cardGrade: {
+    fontSize: '1.1rem',
+    fontWeight: 600,
+  },
+  popupButtonHint: {
+    position: 'fixed',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 8,
+    paddingBottom: 8,
+    bottom: (10 + 55) + 15,
+    right: (10) + 5,
+    borderRadius: '3px',
+    backgroundColor: 'white',
+    zIndex: 2,
+  },
+  popupButtonHintArrow: {
+    bottom: '-10px',
+    right: 15,
+    border: '5px solid white',
+    content: '',
+    position: 'absolute',
+    borderBottomColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderTopColor: 'white',
+    borderLeftColor: 'transparent'
+  },
+  popupButton: {
+    cursor: 'pointer',
+    position: 'fixed',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    // border: `1px solid ${colors.primary}`,
+    width: 55,
+    height: 55,
+    border: '1px solid rgba(0,0,0,0)',
+    // right: window.innerHeight - 100,
+    // top: window.innerWidth - 100
+    bottom: 10,
+    right: 10,
+    borderRadius: '50%',
+    zIndex: 2,
+    backgroundColor: colors.primaryLight
+  }
 }
 export default State;
