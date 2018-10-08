aec47968345d3c6c6ed6a0a148604f13713b7d31
dpett122 <dpett122@gmail.com>
2018-10-08 15:38:47 -0400

    scrolltotop, changing homepage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import UsMap from './pages/Map.js'
 import State from './pages/State.js'
 import About from './pages/About.js'
 import Contact from './pages/Contact.js'
-
+import ScrollToTop from './components/ScrollToTop'
 
 // APP
 // for server rendering: https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#serving-apps-with-client-side-routing
@@ -16,15 +16,17 @@ const App = () => {
   return (
     // delete when deploying to custom URL
     <Router>
-      <div>
+      <ScrollToTop>
         <NavHeader />
         <Route path='/State/:id' exact component={State} />
         <Route path='/About' exact component={About} />
         <Route path='/Contact' exact component={Contact} />
         <Route exact path='/' component={UsMap} />
-      </div>
+      </ScrollToTop>
     </Router>
   );
 };
+
+
 
 export default App;
